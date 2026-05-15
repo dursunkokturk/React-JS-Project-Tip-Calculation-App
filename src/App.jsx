b78d28ba575431296b04a3ef7d740f4ffa68184c
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-15 21:43:48 +0300

    src klasoru icindeki app.jsx component icinde genel baslik div'i olusturuldu, container div'i icinde left ve right div'leri olusturuldu, left div'i icinde baslik ve input elementleri eklendi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
 
 export default function App() {
-  
+
   return (
     <>
+      <div className="general-title">
+        <h1>AYIRICI</h1>
+      </div>
+      <div className="container">
+        <div className="left">
+          <h4>Hesap</h4>
+          <input type="text" />
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
     </>
   )
 }
