089dd11e14f0577a990077cf050979b2c597fd94
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-16 11:31:29 +0300

    src klasoru icindeki app.jsx component icinde container div'i icindeki left div'i icindeki form-group div'i icinde app.css dosyasinda css duzenlemesi yapildi, container div'i icindeki tip-select-group div alani olusturuldu, app.css dosyasinda css islemleri yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,18 @@ export default function App() {
             <h4>Hesap</h4>
             <div className="input-box">
               <span className='icon'>₺</span>
-              <input type="text" placeholder='Ücret Giriniz'/>
+              <input type="text" placeholder='Ücret Giriniz' />
+            </div>
+          </div>
+          <div className="tip-select-group">
+            <h4>Bahşiş Oranı Seç %</h4>
+            <div className="tip-group">
+              <label htmlFor="">% 5</label>
+              <label htmlFor="">% 10</label>
+              <label htmlFor="">% 15</label>
+              <label htmlFor="">% 25</label>
+              <label htmlFor="">% 50</label>
+              <label htmlFor="" className='custom'>Özel</label>
             </div>
           </div>
         </div>
